76464280fbeaccd7072cb88e0ebb6ad50b29335d
Administrator <729696578@qq.com>
2018-04-13 10:08:14 +0800

    日志入库

--- log-console-admin/src/main/java/info/logconsole/admin/mqreceiver/LogMQReceiver.java
+++ log-console-admin/src/main/java/info/logconsole/admin/mqreceiver/LogMQReceiver.java
@@ -8,10 +8,19 @@
 
 package info.logconsole.admin.mqreceiver;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import javax.annotation.Resource;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
-
+import com.alibaba.fastjson.JSONObject;
+import info.logconsole.admin.entity.LogRecordInfo;
+import info.logconsole.admin.mapper.LogRecordInfoMapper;
 import info.logconsole.appender.MQAppenderConsts;
+import info.logconsole.appender.model.LogMessage;
 
 /**
  * ClassName:LogMQReceiver <br/>
@@ -21,12 +30,25 @@ import info.logconsole.appender.MQAppenderConsts;
  */
 @Component
 public class LogMQReceiver {
+	
+	@Resource
+	private LogRecordInfoMapper logRecordInfoMapper;
 
 	@JmsListener(destination = MQAppenderConsts.TOPIC)
 	public void receiveMQLogMessage(String logMessage) {
 		
-		System.out.println("log message is ==> " + logMessage);
+		LogMessage logMsg = JSONObject.parseObject(logMessage, LogMessage.class);
 		
+		LogRecordInfo logRecordInfo = new LogRecordInfo();
+		logRecordInfo.setSystemId(logMsg.getAppName());
+		logRecordInfo.setClazz(logMsg.getClazz());
+		logRecordInfo.setLogLevel(logMsg.getLevel());
+		logRecordInfo.setLogContent(logMsg.getLog());
+		logRecordInfo.setCreateDate(DateFormatUtils.format(new Date(), "yyyyMMddHHmmss"));
+		logRecordInfo.setLogTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(logMsg.getTimestamp()), 
+										ZoneId.systemDefault()));
+		
+		logRecordInfoMapper.insert(logRecordInfo);
 	}
 }
 
